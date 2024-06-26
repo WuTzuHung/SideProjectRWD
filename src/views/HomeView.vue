1600634c60852bd129c7b99eefa49bdb6d6597e0
WuTzuHung <not5508085@gmail>
2024-06-26 15:24:56 +0800

    ChangeClassName

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -22,13 +22,13 @@ export default {
 
 <template>
 
-  <div class="big"></div>
+  <div class="Container"></div>
 
 </template>
 
 <style>
 
-.big{
+.Container{
   width: 100%;
   height: 85dvh;
   background-color: purple;
@@ -37,7 +37,7 @@ export default {
 /* iPad-PC: 768px - 991px */
 @media (min-width: 768px) {
     /* iPad 或較小的桌面專用的 CSS */
-    .big{
+    .Container{
   background-color:yellow;
 }
 }
@@ -45,7 +45,7 @@ export default {
 /* PC: 992px 以上 */
 @media (min-width: 992px) {
     /* 電腦專用的 CSS */
-    .big {
+    .Container {
         background-color: green; /* 電腦上的背景顏色 */
     }
 }
