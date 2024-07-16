80ea1d66e4dad6c0c203f3f120928e0d42cc2e0a
WuTzuHung <not5508085@gmail>
2024-07-16 23:31:38 +0800

    0716_1131

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -87,7 +87,7 @@ const audioResourceList = ref([
   .tab-content {
     box-sizing: border-box; /* 包含內邊距與邊框 */
     width: 80%;
-    height: 65dvh;
+    height: 105dvh;
     padding: 0.1px;
     margin: 0%;
   /* border: 1px solid #121111;  */
